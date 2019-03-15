a5c871fb3d284a5de79b4cc17c9a10ef6adc3481
Arnold Ruiz <arnoldruiz115@csu.fullerton.edu>
2019-03-15 15:04:21 -0700

    sprite

--- Maze.py
+++ Maze.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+from pygame.sprite import Sprite
 
 pygame.init()
 
@@ -7,12 +8,34 @@ pygame.init()
 window = pygame.display.set_mode((1000, 500))  # set window size
 
 
-class Player:
-    px = 0
-    py = 0
-    speed = 1
-    p_width = 20
+class Player(Sprite):
+    def __init__(self):
+        self.px = 0
+        self.py = 0
+        self.speed = 1
+        self.p_width = 20
+        self.rect = pygame.rect(self.px, self.py, self.p_width, self.p_width)
 
+    def draw_player(self):
+        pygame.draw.rect(window, (240, 230, 140), self.rect)  # Draw player every frame
+
+
+class Wall(Sprite):
+    def __init__(self):
+        self.p_width = 10
+        self.rect = pygame.rect(40, 40, 10, 80)
+
+    def draw_wall(self):
+        pygame.draw.rect(window, (240, 140, 230), self.rect)
+
+
+def check_collision(sprite1, sprite2):
+    col = pygame.sprite.collide_rect(sprite1, sprite2)
+    if col == True:
+        print("Collision")
+
+
+wall = Wall
 
 main = Player
 clock = pygame.time.Clock()
@@ -32,14 +55,13 @@ while True:
     if keys[K_DOWN]:
         main.py += main.speed
 
-    if main.px == 98 and main.py < 300:
-        main.px = 97
-    if main.px == 123 and main.py < 300:
-        main.px = 124
+    #check_collision(main, wall)
 
     window.fill((180, 210, 240))  # draw the background
     # Draw black line       (r,g,b), starting (x,y), ending (x,y), width
     pygame.draw.line(window, (20, 20, 20), (120, 0), (120, 300), 5)
-    pygame.draw.rect(window, (240, 230, 140), (main.px, main.py, main.p_width, main.p_width))  # Draw player every frame
+
+    main.draw_player(main)
+    wall.draw_wall(wall)
 
     pygame.display.update()
