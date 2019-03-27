2787ba692a7fc736ac73f67ba93f73190e71c8cb
Ye-Rham <hyerham@gmail.com>
2019-03-27 10:28:23 -0700

    Button added some fixes

---
+++ Functions.py
@@ -0,0 +1,38 @@
+import pygame
+import sys
+
+
+def check_keydown_events(event, main):
+    if event.key == pygame.K_LEFT:
+        main.px -= main.speed
+    elif event.key == pygame.K_RIGHT:
+        main.px += main.speed
+    elif event.key == pygame.K_UP:
+        main.py -= main.speed
+    elif event.key == pygame.K_DOWN:
+        main.py += main.speed
+
+
+def check_events(play_button, main):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(play_button, mouse_x, mouse_y)
+        elif event.type == pygame.MOUSEBUTTONUP:
+            play_button.pressed = False
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            main.rect.x -= main.speed
+        if keys[pygame.K_RIGHT]:
+            main.rect.x += main.speed
+        if keys[pygame.K_UP]:
+            main.rect.y -= main.speed
+        if keys[pygame.K_DOWN]:
+            main.rect.y += main.speed
+
+
+def check_play_button(play_button, mouse_x, mouse_y):
+    if play_button.rect.collidepoint(mouse_x, mouse_y):
+        play_button.pressed = True
--- Maze.py
+++ Maze.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame.locals import *
 from pygame.sprite import Sprite
+from Button import Button
+import Functions as f
 
 pygame.init()
 
@@ -12,22 +14,21 @@ class Player(Sprite):
     def __init__(self, window):
         super(Player, self).__init__()
         self.window = window
-        self.px = 0
-        self.py = 0
         self.speed = 1
         self.p_width = 20
         self.rect = pygame.Rect(0, 0, self.p_width, self.p_width)
-        self.rect.x = self.px
-        self.rect.y = self.py
+        self.last_direction = 'r'
 
     def draw_player(self):
         pygame.draw.rect(self.window, (240, 230, 140), self.rect)  # Draw player every frame
 
 
 class Wall(Sprite):
-    def __init__(self):
+    def __init__(self, window):
+        super(Wall, self).__init__()
+        self.window = window
         self.p_width = 10
-        self.rect = pygame.rect(40, 40, 10, 80)
+        self.rect = pygame.Rect(40, 40, 20, 20)
 
     def draw_wall(self):
         pygame.draw.rect(window, (240, 140, 230), self.rect)
@@ -36,36 +37,44 @@ class Wall(Sprite):
 def check_collision(sprite1, sprite2):
     col = pygame.sprite.collide_rect(sprite1, sprite2)
     if col == True:
-        print("Collision")
+        if sprite1.rect.right < sprite2.rect.x + 3 * sprite1.speed:
+            sprite1.rect.right = sprite2.rect.x
+        elif sprite1.rect.x > sprite2.rect.right - 3 * sprite1.speed:
+            sprite1.rect.x = sprite2.rect.right
+        if sprite1.rect.bottom < sprite2.rect.y + 3 * sprite1.speed:
+            sprite1.rect.bottom = sprite2.rect.y
+        elif sprite1.rect.y > sprite2.rect.bottom - 3 * sprite1.speed:
+            sprite1.rect.y = sprite2.rect.bottom
 
 
-wall = Wall
+wall = Wall(window)
 
 main = Player(window)
 clock = pygame.time.Clock()
+start_button = Button(window, "START", 500, 250)
+
 while True:
     clock.tick(120)  # FPS
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
+    f.check_events(start_button, main)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        main.px -= main.speed
+        main.rect.x -= main.speed
     if keys[pygame.K_RIGHT]:
-        main.px += main.speed
+        main.rect.x += main.speed
     if keys[K_UP]:
-        main.py -= main.speed
+        main.rect.y -= main.speed
     if keys[K_DOWN]:
-        main.py += main.speed
+        main.rect.y += main.speed
 
-    #check_collision(main, wall)
+    check_collision(main, wall)
 
     window.fill((180, 210, 240))  # draw the background
     # Draw black line       (r,g,b), starting (x,y), ending (x,y), width
     pygame.draw.line(window, (20, 20, 20), (120, 0), (120, 300), 5)
 
     main.draw_player()
-    wall.draw_wall(wall)
+    wall.draw_wall()
+    start_button.draw()
 
     pygame.display.update()
