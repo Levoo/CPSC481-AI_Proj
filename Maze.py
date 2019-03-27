197192d40aed3064e17b4c63cdbeb9220ac88aa0
Ye-Rham <hyerham@gmail.com>
2019-03-27 10:36:23 -0700

    Small changes

--- Maze.py
+++ Maze.py
@@ -10,16 +10,17 @@ pygame.init()
 window = pygame.display.set_mode((1000, 500))  # set window size
 
 
-class Player(Sprite):
+class Unit(Sprite):
     def __init__(self, window):
-        super(Player, self).__init__()
+        super(Unit, self).__init__()
         self.window = window
         self.speed = 1
         self.p_width = 20
         self.rect = pygame.Rect(0, 0, self.p_width, self.p_width)
         self.last_direction = 'r'
+        self.last_position = ''
 
-    def draw_player(self):
+    def draw(self):
         pygame.draw.rect(self.window, (240, 230, 140), self.rect)  # Draw player every frame
 
 
@@ -30,13 +31,13 @@ class Wall(Sprite):
         self.p_width = 10
         self.rect = pygame.Rect(40, 40, 20, 20)
 
-    def draw_wall(self):
+    def draw(self):
         pygame.draw.rect(window, (240, 140, 230), self.rect)
 
 
 def check_collision(sprite1, sprite2):
     col = pygame.sprite.collide_rect(sprite1, sprite2)
-    if col == True:
+    if col:
         if sprite1.rect.right < sprite2.rect.x + 3 * sprite1.speed:
             sprite1.rect.right = sprite2.rect.x
         elif sprite1.rect.x > sprite2.rect.right - 3 * sprite1.speed:
@@ -49,7 +50,7 @@ def check_collision(sprite1, sprite2):
 
 wall = Wall(window)
 
-main = Player(window)
+main = Unit(window)
 clock = pygame.time.Clock()
 start_button = Button(window, "START", 500, 250)
 
@@ -73,8 +74,8 @@ while True:
     # Draw black line       (r,g,b), starting (x,y), ending (x,y), width
     pygame.draw.line(window, (20, 20, 20), (120, 0), (120, 300), 5)
 
-    main.draw_player()
-    wall.draw_wall()
+    main.draw()
+    wall.draw()
     start_button.draw()
 
     pygame.display.update()
