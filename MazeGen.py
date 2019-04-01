ea8253f201343e30161a01274d5e1c4bedd5409d
Arnold Ruiz <arnoldruiz115@csu.fullerton.edu>
2019-04-01 14:30:23 -0700

    updated get rand/next node and dfs

--- MazeGen.py
+++ MazeGen.py
@@ -1,5 +1,6 @@
 import enum
 import random
+import queue
 
 # WIP
 
@@ -15,6 +16,11 @@ for x in range(0, 1305, 50):
         #print("placing line on x: " + str(x))
 
 '''
+
+
+
+
+
 #enum for sides, used for self.walls 
 class sides(enum.Enum):
     top = 0
@@ -48,7 +54,8 @@ class singleCell:
 
 class mazeGen:
     def __init__(self, cellObj):
-        self.grid = [ [cellObj for x in range(5)] for y in range(5) ] # n by m matrix currently 5x5
+        self.size = 5
+        self.grid = [ [cellObj for x in range(self.size)] for y in range(self.size) ] # n by m matrix currently 5x5
         self.cell_queue = 0
         self.visitedCount = 0
 
@@ -65,16 +72,47 @@ class mazeGen:
     Go to step 2.
     '''
     def depth_first_search_maze_gen(self):
+        dfsQ = queue.Queue(maxsize=self.size*self.size)
+
+        while self.visitedCount != (self.size * self.size):
+            self.grid[0][0].set_node_coord(0, 0)    # starting cell is at (0, 0)
+            dfsQ.put(self.grid[0][0])               # put current cell on queue
+
+            nextCell = self.get_rand_node(self.grid[0][0])  # check adjacent cells if visited and get next rand cell
+            dfsQ.put(nextCell)
+
+        '''
         x, y = self.get_rand_node() #get random cell in self.grid
         self.grid[x][y].set_node_coord(x, y) # have the grid x,y be stored by the obj thats in that position
         print ("X: " + str(x) + "| Y: " + str(y))  # rest is code to make sure it works 
         self.grid[x][y].print_node()
         self.grid[x][y].set_visited_status(True)
         self.grid[x][y].print_node()
+        '''
 
-    # find a random x and y value between 0-4 bc this is a 5x5 grid (plane to not be hardcoded)
-    def get_rand_node(self):
-        return random.randint(0,4), random.randint(0,4)
+    #  TODO: Check if adjacent cells have been visited
+
+    def get_rand_node(self, tempCell):
+
+        direction = random.randint(0, 3)
+        if direction == 0:  # top
+            nextX = tempCell.x + 0
+            nextY = tempCell.y + 1
+        if direction == 1:  # bottom
+            nextX = tempCell.x + 0
+            nextY = tempCell.y - 1
+        if direction == 2:  # right
+            nextX = tempCell.x + 1
+            nextY = tempCell.y + 0
+        if direction == 3:  # left
+            nextX = tempCell.x - 1
+            nextY = tempCell.y + 0
+
+
+        nextCell = singleCell()
+        nextCell.set_node_coord(nextX, nextY)
+        return nextCell
+
 
 cells = singleCell()
 genn = mazeGen(cells)
