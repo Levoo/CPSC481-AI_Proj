ddb687b47f3a412860f0f4473ab7ba7c2982b5d7
Ye-Rham <hyerham@gmail.com>
2019-05-01 20:19:18 -0700

    Fixed

--- Unit.py
+++ Unit.py
@@ -49,8 +49,8 @@ class Unit(Sprite):
             self.rect = self.rect.move(self.speed, 0)
 
         # Next move if node is hit
-        if self.rect.y == self.next_node.x * self.p_width + self.p_width and \
-                self.rect.x == self.next_node.y * self.p_width + self.p_width:
+        if self.rect.y == self.next_node.x * self.p_width * 2 + self.p_width and \
+                self.rect.x == self.next_node.y * self.p_width * 2ing  + self.p_width:
             self.visited_nodes.append(self.next_node)
             self.current_node = self.next_node
             if self.current_node.beenVisited[self.id_no][0] is False:
